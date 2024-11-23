9a7914ce343951dd8b61eddb72a3d17da50c5fdb
gomgun-lab <zxolqs@gmail.com>
2024-11-23 21:27:06 +0900

    update export css filename

--- apps/storybook/.storybook/preview.tsx
+++ apps/storybook/.storybook/preview.tsx
@@ -1,9 +1,7 @@
-import "@topinerd/preset/theme.css";
-import "@topinerd/react/index.css";
-import { theme } from "@topinerd/preset";
+import "@topinerd-design-system/preset/theme.css";
+import "@topinerd-design-system/react/style.css";
 
 import type { Preview } from "@storybook/react";
-import React from "react";
 
 const preview: Preview = {
   parameters: {
@@ -16,32 +14,32 @@ const preview: Preview = {
   },
 };
 
-export const decorators = [
-  (Story, context) => {
-    const themeClass =
-      context.globals.theme === "dark" ? theme.darkTheme : theme.lightTheme;
-    return (
-      <div className={themeClass}>
-        <Story />
-      </div>
-    );
-  },
-];
+// export const decorators = [
+//   (Story, context) => {
+//     const themeClass =
+//       context.globals.theme === "dark" ? theme.mode.dark : theme.mode.light;
+//     return (
+//       <div className={themeClass}>
+//         <Story />
+//       </div>
+//     );
+//   },
+// ];
 
-export const globalTypes = {
-  theme: {
-    name: "Theme",
-    description: "Global theme for components",
-    defaultValue: "light",
-    toolbar: {
-      icon: "circlehollow",
-      items: [
-        { value: "light", title: "Light" },
-        { value: "dark", title: "Dark" },
-      ],
-      showName: true,
-    },
-  },
-};
+// export const globalTypes = {
+//   theme: {
+//     name: "Theme",
+//     description: "Global theme for components",
+//     defaultValue: "light",
+//     toolbar: {
+//       icon: "circlehollow",
+//       items: [
+//         { value: "light", title: "Light" },
+//         { value: "dark", title: "Dark" },
+//       ],
+//       showName: true,
+//     },
+//   },
+// };
 
 export default preview;
