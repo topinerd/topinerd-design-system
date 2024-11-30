cd602ba74fa1143834885993d5ff1079d74a4290
gomgun-lab <zxolqs@gmail.com>
2024-11-30 22:37:18 +0900

    remove root lint, prettier file

--- .prettierrc.cjs
+++
@@ -1,7 +0,0 @@
-const topinerdPrettierConfig = require("@topinerd-design-system/prettier-config");
-
-/** @type {import("prettier").Config} */
-module.exports = {
-  ...topinerdPrettierConfig,
-};
-
--- eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { config } from "@topinerd-design-system/eslint-config/tseslint-react-internal";
-
-/** @type {import("eslint").Linter.Config} */
-export default config;
